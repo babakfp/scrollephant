0eaaf2af5dc58f80389e700861ca3363068fec4a
Babak Farkhoopak <babak.bxf@gmail.com>
2023-05-07 22:50:48 -0700

    Updated Prettier config

---
+++ .prettierrc.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+    semi: false,
+    tabWidth: 4,
+    arrowParens: "avoid",
+    overrides: [
+        { files: "*.svelte", options: { parser: "svelte" } },
+        { files: "*.md", options: { tabWidth: 2 } },
+    ],
+    plugins: [
+        require("prettier-plugin-svelte"),
+    ],
+    pluginSearchDirs: ["."],
+}
