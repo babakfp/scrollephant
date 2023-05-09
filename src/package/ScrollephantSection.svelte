4b154e41715126998e7dfe3026bda66dceba21ca
Babak Farkhoopak <babak.bxf@gmail.com>
2023-05-09 00:27:10 -0700

    Added transform

--- src/package/ScrollephantSection.svelte
+++ src/package/ScrollephantSection.svelte
@@ -14,9 +14,11 @@
     :global(.scrollephant[data-direction="vertical"]) .scrollephant-section {
         height: 100vh;
         height: 100dvh;
+        transform: translateY(--scrollephant-translate-y);
     }
     :global(.scrollephant[data-direction="horizontal"]) .scrollephant-section {
         width: 100vw;
         width: 100dvw;
+        transform: translateX(--scrollephant-translate-x);
     }
 </style>
