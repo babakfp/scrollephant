2976ee547e48489a05111b2602aa61564de77c6c
Babak Farkhoopak <babak.bxf@gmail.com>
2023-05-08 22:35:55 -0700

    Stuff

--- src/package/Scrollephant.svelte
+++ src/package/Scrollephant.svelte
@@ -3,28 +3,28 @@
     import { writable } from "svelte/store"
 
     export let direction: "vertical" | "horizontal" = "vertical"
-    // export let activeSectionNumber = 1
+    export let activeSectionNumber = 1
 
     const numberOfSections = setContext("numberOfSections", writable(0))
 
-    // function isMovingForward(e: WheelEvent) {
-    //     if (e.deltaY > 0) return true
-    //     if (e.deltaY < 0) return false
-    //     throw Error("Invalid. Moving nowhere!")
-    // }
+    function isMovingForward(e: WheelEvent) {
+        if (e.deltaY > 0) return true
+        if (e.deltaY < 0) return false
+        throw Error("Invalid. Moving nowhere!")
+    }
 
     function handleMousewheel(e: WheelEvent) {
-        // console.log($numberOfSections)
-        //     if (isMovingForward(e)) {
-        //         if (activeSectionNumber <= $numberOfSections) {
-        //             activeSectionNumber += 1
-        //         }
-        //     } else {
-        //         if (activeSectionNumber >= $numberOfSections) {
-        //             activeSectionNumber -= 1
-        //         }
-        //     }
-        //     console.log(activeSectionNumber)
+        console.log(isMovingForward(e))
+
+        if (isMovingForward(e)) {
+            if (activeSectionNumber <= $numberOfSections) {
+                activeSectionNumber += 1
+            }
+        } else {
+            if (activeSectionNumber >= $numberOfSections) {
+                activeSectionNumber -= 1
+            }
+        }
     }
 </script>
 
