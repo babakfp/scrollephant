1b4423d616767853f3f50d54e415b5e0178f0bc7
Babak Farkhoopak <babak.bxf@gmail.com>
2023-05-11 11:13:31 -0700

    Changed tooltip styles

--- src/package/ScrollephantTooltip.svelte
+++ src/package/ScrollephantTooltip.svelte
@@ -12,11 +12,11 @@
         transform: translateX(-0.75rem) translateY(-50%);
         background: white;
         border-radius: 0.5rem;
-        padding: 0.25rem 0.75rem;
+        padding: 0.5rem 0.75rem;
         white-space: nowrap;
         font-size: 0.75rem;
         opacity: 0;
-        transition: 100ms ease-in;
+        transition: 200ms ease-in;
         pointer-events: none;
         z-index: -1;
     }
