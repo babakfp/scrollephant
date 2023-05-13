c936c328b5153ccc5ed052af85e8683f8efa7b15
Babak Farkhoopak <babak.bxf@gmail.com>
2023-05-13 08:12:03 -0700

    Fixed tooltip horizontal

--- src/package/ScrollephantTooltip.svelte
+++ src/package/ScrollephantTooltip.svelte
@@ -7,9 +7,6 @@
 <style>
     div {
         position: absolute;
-        right: 100%;
-        top: 50%;
-        transform: translateX(-0.75rem) translateY(-50%);
         background: white;
         border-radius: 0.5rem;
         padding: 0.5rem 0.75rem;
@@ -20,9 +17,18 @@
         pointer-events: none;
         z-index: -1;
     }
-
     :global(.scrollephant-dots button:hover) + div {
         opacity: 1;
+    }
+
+    :global(.scrollephant[data-direction="vertical"]) div {
+        right: 100%;
+        top: 50%;
         transform: translateX(-0.75rem) translateY(-50%);
+    }
+    :global(.scrollephant[data-direction="horizontal"]) div {
+        bottom: 100%;
+        left: 50%;
+        transform: translateX(-50%) translateY(-0.75rem);
     }
 </style>
