fa8f688ec6c1a5a0b43b627cffb769dd4b079b02
Babak Farkhoopak <babak.bxf@gmail.com>
2023-05-14 03:24:24 -0700

    Added loop demo

---
+++ src/routes/loop/+page.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    import {
+        Scrollephant,
+        ScrollephantWrapper,
+        ScrollephantSection,
+        ScrollephantDots,
+        ScrollephantButtons,
+    } from "scrollephant"
+</script>
+
+<Scrollephant loopFromStart={true} loopFromEnd={true}>
+    <ScrollephantWrapper>
+        <ScrollephantSection label="Section 1">
+            <div class="section-inner">1</div>
+        </ScrollephantSection>
+        <ScrollephantSection label="Section 2">
+            <div class="section-inner">2</div>
+        </ScrollephantSection>
+        <ScrollephantSection label="Section 3">
+            <div class="section-inner">3</div>
+        </ScrollephantSection>
+    </ScrollephantWrapper>
+
+    <ScrollephantButtons />
+    <ScrollephantDots />
+</Scrollephant>
