a9b940245e42fd2fd698e6b22a2b4b8cc10ffcd9
babakfp <babak.bxf@gmail.com>
2023-07-11 21:52:53 -0700

    Demo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,20 +8,20 @@
     } from "scrollephant"
 </script>
 
-<Scrollephant>
+<Scrollephant loopDown={true} loopUp={true}>
     <ScrollephantWrapper>
         <ScrollephantSection label="Section 1">
             <div class="section-inner">1</div>
         </ScrollephantSection>
         <ScrollephantSection label="Section 2" isSubSectionWrapper={true}>
-            <ScrollephantSubSection label="Section 1">
-                <div class="section-inner">1</div>
+            <ScrollephantSubSection label="Section 2.1">
+                <div class="section-inner">2.1</div>
             </ScrollephantSubSection>
-            <ScrollephantSubSection label="Section 2">
-                <div class="section-inner">2</div>
+            <ScrollephantSubSection label="Section 2.2">
+                <div class="section-inner">2.2</div>
             </ScrollephantSubSection>
-            <ScrollephantSubSection label="Section 3">
-                <div class="section-inner">3</div>
+            <ScrollephantSubSection label="Section 2.3">
+                <div class="section-inner">2.3</div>
             </ScrollephantSubSection>
         </ScrollephantSection>
         <ScrollephantSection label="Section 3">
