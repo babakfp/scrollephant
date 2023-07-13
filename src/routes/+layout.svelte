eb9b7c9f805134f187f6bb34bc3354578b4944fe
babakfp <babak.bxf@gmail.com>
2023-07-12 21:48:55 -0700

    Updated colors

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -27,22 +27,31 @@
     :global(.scrollephant-section) {
         background: #52525b;
     }
-    :global(.scrollephant-section:nth-child(1)) {
+    :global(.scrollephant-section:nth-child(1)),
+    :global(.scrollephant-subsection:nth-child(1)) {
         background: #ff5f45;
     }
-    :global(.scrollephant-section:nth-child(2)) {
+    :global(.scrollephant-section:nth-child(2)),
+    :global(.scrollephant-subsection:nth-child(2)) {
         background: #0798ec;
     }
-    :global(.scrollephant-section:nth-child(3)) {
+    :global(.scrollephant-section:nth-child(3)),
+    :global(.scrollephant-subsection:nth-child(3)) {
         background: #fec401;
     }
-    :global(.scrollephant-section:nth-child(4)) {
+    :global(.scrollephant-section:nth-child(4)),
+    :global(.scrollephant-subsection:nth-child(4)) {
         background: #65a30d;
     }
-    :global(.scrollephant-section:nth-child(5)) {
+    :global(.scrollephant-section:nth-child(5)),
+    :global(.scrollephant-subsection:nth-child(5)) {
         background: #0891b2;
     }
-    :global(.scrollephant-section:nth-child(6)) {
+    :global(.scrollephant-section:nth-child(6)),
+    :global(.scrollephant-subsection:nth-child(6)) {
         background: #7c3aed;
+    }
+    :global(.scrollephant-section[data-scrollephant-have-subsection="true"]) {
+        background: unset;
     }
 </style>
