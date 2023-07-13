6c80212e4acbe2c98545c74d317693bd27bc2a99
babakfp <babak.bxf@gmail.com>
2023-07-12 20:22:07 -0700

    Updated demo

--- src/routes/arrow-navigation/+page.svelte
+++ src/routes/arrow-navigation/+page.svelte
@@ -9,12 +9,13 @@
     } from "scrollephant"
 </script>
 
-<Scrollephant movement="fade">
+<Scrollephant>
     <ScrollephantWrapper>
         <ScrollephantSection label="Section 1">1</ScrollephantSection>
         <ScrollephantSection label="Section 2">2</ScrollephantSection>
         <ScrollephantSection label="Section 3">3</ScrollephantSection>
     </ScrollephantWrapper>
+
     <ScrollephantNavigationsWrapper>
         <ScrollephantButtonPrev />
         <ScrollephantButtonNext />
