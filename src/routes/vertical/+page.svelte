16ce957221924249ff6a1fb6c8604fed4fe29e3b
babakfp <babak.bxf@gmail.com>
2023-07-12 20:48:31 -0700

    Added vertical demo

---
+++ src/routes/vertical/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import {
+        Scrollephant,
+        ScrollephantWrapper,
+        ScrollephantSection,
+    } from "scrollephant"
+</script>
+
+<Scrollephant>
+    <ScrollephantWrapper>
+        <ScrollephantSection label="Section 1">1</ScrollephantSection>
+        <ScrollephantSection label="Section 2">2</ScrollephantSection>
+        <ScrollephantSection label="Section 3">3</ScrollephantSection>
+    </ScrollephantWrapper>
+</Scrollephant>
