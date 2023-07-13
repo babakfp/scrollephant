d86bd43b890085f45a581df6089cf2bb51161355
babakfp <babak.bxf@gmail.com>
2023-07-12 20:12:33 -0700

    Added auto-height-last-loop and auto-height-first-loop demos

---
+++ src/routes/auto-height-first-loop/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import {
+        Scrollephant,
+        ScrollephantWrapper,
+        ScrollephantSection,
+    } from "scrollephant"
+</script>
+
+<Scrollephant loopUp={true} loopDown={true}>
+    <ScrollephantWrapper>
+        <ScrollephantSection label="Auto Height" autoHeight={true}>
+            Auto Height
+        </ScrollephantSection>
+        <ScrollephantSection label="Section 2">2</ScrollephantSection>
+        <ScrollephantSection label="Section 3">3</ScrollephantSection>
+    </ScrollephantWrapper>
+</Scrollephant>
