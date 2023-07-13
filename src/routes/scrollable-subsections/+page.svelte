c34e21e1cbca7a181760c900b16304c54479a448
babakfp <babak.bxf@gmail.com>
2023-07-12 20:44:26 -0700

    Added 2 subsections demos

---
+++ src/routes/scrollable-subsections/+page.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+    import {
+        Scrollephant,
+        ScrollephantWrapper,
+        ScrollephantSection,
+        ScrollephantSubSection,
+        ScrollephantNavigationsWrapper,
+        ScrollephantButtonPrev,
+        ScrollephantDots,
+        ScrollephantButtonNext,
+    } from "scrollephant"
+</script>
+
+<Scrollephant scrollableSubSections={true}>
+    <ScrollephantWrapper>
+        <ScrollephantSection label="Section 1">1</ScrollephantSection>
+        <ScrollephantSection label="Section 2">
+            <ScrollephantSubSection label="Section 2.1">
+                2.1
+            </ScrollephantSubSection>
+            <ScrollephantSubSection label="Section 2.2">
+                2.2
+            </ScrollephantSubSection>
+            <ScrollephantSubSection label="Section 2.3">
+                2.3
+            </ScrollephantSubSection>
+        </ScrollephantSection>
+        <ScrollephantSection label="Section 3">3</ScrollephantSection>
+    </ScrollephantWrapper>
+</Scrollephant>
