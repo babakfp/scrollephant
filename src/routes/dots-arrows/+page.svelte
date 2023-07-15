707b720facbc9ed77197758bad591c571763bf2a
babakfp <babak.bxf@gmail.com>
2023-07-15 03:17:46 -0700

    Added dots-arrows demo

---
+++ src/routes/dots-arrows/+page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    import {
+        Scrollephant,
+        ScrollephantWrapper,
+        ScrollephantSection,
+        ScrollephantNavigationsWrapper,
+        ScrollephantDots,
+        ScrollephantButtonPrev,
+        ScrollephantButtonNext,
+    } from "scrollephant"
+</script>
+
+<Scrollephant>
+    <ScrollephantWrapper>
+        <ScrollephantSection label="Section 1">1</ScrollephantSection>
+        <ScrollephantSection label="Section 2">2</ScrollephantSection>
+        <ScrollephantSection label="Section 3">3</ScrollephantSection>
+    </ScrollephantWrapper>
+
+    <ScrollephantNavigationsWrapper>
+        <ScrollephantButtonPrev />
+        <ScrollephantDots />
+        <ScrollephantButtonNext />
+    </ScrollephantNavigationsWrapper>
+</Scrollephant>
