a3142bdc148d8f7df8684d78732819b7c84ff739
babakfp <babak.bxf@gmail.com>
2023-07-15 05:32:59 -0700

    Added scrollable-subsections-dots-arrows demo

---
+++ src/routes/scrollable-subsections-dots-arrows/+page.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+    import {
+        Scrollephant,
+        ScrollephantWrapper,
+        ScrollephantSection,
+        ScrollephantSubSection,
+        ScrollephantNavigationsWrapper,
+        ScrollephantDots,
+        ScrollephantButtonPrev,
+        ScrollephantButtonNext,
+    } from "scrollephant"
+</script>
+
+<Scrollephant>
+    <ScrollephantWrapper>
+        <ScrollephantSection label="Section 1">1</ScrollephantSection>
+        <ScrollephantSection label="Section 2">
+            <ScrollephantSubSection label="Section 2.1">
+                2.1
+            </ScrollephantSubSection>
+            <ScrollephantSubSection label="Section 2.2">
+                2.2
+            </ScrollephantSubSection>
+            <ScrollephantSubSection label="Section 2.3">
+                2.3
+            </ScrollephantSubSection>
+        </ScrollephantSection>
+        <ScrollephantSection label="Section 3">3</ScrollephantSection>
+    </ScrollephantWrapper>
+
+    <ScrollephantNavigationsWrapper>
+        <ScrollephantButtonPrev />
+        <ScrollephantDots />
+        <ScrollephantButtonNext />
+    </ScrollephantNavigationsWrapper>
+</Scrollephant>
