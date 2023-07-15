7af8fdd15a6a67e2249369824043430454cda7af
babakfp <babak.bxf@gmail.com>
2023-07-15 03:00:01 -0700

    Added non-scrollable-subsections demo

---
+++ src/routes/non-scrollable-subsections/+page.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    import {
+        Scrollephant,
+        ScrollephantWrapper,
+        ScrollephantSection,
+        ScrollephantSubSection,
+    } from "scrollephant"
+</script>
+
+<Scrollephant scrollableSubSections={false}>
+    <ScrollephantWrapper>
+        <ScrollephantSection label="Section 1">1</ScrollephantSection>
+        <ScrollephantSection label="Section 2">
+            <ScrollephantSubSection label="Section 2.1">
+                2.1
+            </ScrollephantSubSection>
+            <ScrollephantSubSection label="Section 2.2">
+                2.2
+            </ScrollephantSubSection>
+            <ScrollephantSubSection label="Section 2.3">
+                2.3
+            </ScrollephantSubSection>
+        </ScrollephantSection>
+        <ScrollephantSection label="Section 3">3</ScrollephantSection>
+    </ScrollephantWrapper>
+</Scrollephant>
