1c52ff9eb138c58bb276bf6c8bf076b6a66edb1e
babakfp <babak.bxf@gmail.com>
2023-07-18 07:07:31 -0700

    Added horizontal-arrows-dots demo

---
+++ src/routes/horizontal-arrows-dots/+page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    import {
+        Scrollephant,
+        ScrollephantWrapper,
+        ScrollephantSection,
+        ScrollephantNavigationsWrapper,
+        ScrollephantArrowPrev,
+        ScrollephantArrowNext,
+        ScrollephantDots,
+    } from "scrollephant"
+</script>
+
+<Scrollephant direction="horizontal">
+    <ScrollephantWrapper>
+        <ScrollephantSection label="Section 1">1</ScrollephantSection>
+        <ScrollephantSection label="Section 2">2</ScrollephantSection>
+        <ScrollephantSection label="Section 3">3</ScrollephantSection>
+    </ScrollephantWrapper>
+
+    <ScrollephantNavigationsWrapper>
+        <ScrollephantArrowPrev />
+        <ScrollephantDots />
+        <ScrollephantArrowNext />
+    </ScrollephantNavigationsWrapper>
+</Scrollephant>
