e74a6c90afda7ef622d9375a67a546d1acdb8ea2
babakfp <babak.bxf@gmail.com>
2023-07-18 07:02:58 -0700

    Added demos and renamed demos

---
+++ src/routes/horizontal-dots/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    import {
+        Scrollephant,
+        ScrollephantWrapper,
+        ScrollephantSection,
+		ScrollephantNavigationsWrapper,
+        ScrollephantDots,
+    } from "scrollephant"
+</script>
+
+<Scrollephant direction="horizontal">
+    <ScrollephantWrapper>
+        <ScrollephantSection label="Section 1">1</ScrollephantSection>
+        <ScrollephantSection label="Section 2">2</ScrollephantSection>
+        <ScrollephantSection label="Section 3">3</ScrollephantSection>
+    </ScrollephantWrapper>
+
+    <ScrollephantNavigationsWrapper>
+        <ScrollephantDots />
+    </ScrollephantNavigationsWrapper>
+</Scrollephant>
