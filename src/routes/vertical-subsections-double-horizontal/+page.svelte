d168ccff4ef4098c0371c33975e27f936806c9db
babakfp <babak.bxf@gmail.com>
2023-07-21 13:21:51 -0700

    Added demo

---
+++ src/routes/vertical-subsections-double-horizontal/+page.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+    import {
+        Scrollephant,
+        ScrollephantWrapper,
+        ScrollephantSection,
+        ScrollephantSubSection,
+        ScrollephantNavigationsWrapper,
+        ScrollephantArrowPrev,
+        ScrollephantArrowNext,
+        ScrollephantDots,
+    } from "scrollephant"
+</script>
+
+<Scrollephant>
+    <ScrollephantWrapper>
+        <ScrollephantSection label="Section 1">
+            <ScrollephantWrapper>
+                <ScrollephantSubSection label="Section 1.1">
+                    1.1
+                </ScrollephantSubSection>
+                <ScrollephantSubSection label="Section 1.2">
+                    1.2
+                </ScrollephantSubSection>
+            </ScrollephantWrapper>
+        </ScrollephantSection>
+        <ScrollephantSection label="Section 2">
+            <ScrollephantWrapper>
+                <ScrollephantSubSection label="Section 2.1">
+                    2.1
+                </ScrollephantSubSection>
+                <ScrollephantSubSection label="Section 2.2">
+                    2.2
+                </ScrollephantSubSection>
+            </ScrollephantWrapper>
+        </ScrollephantSection>
+    </ScrollephantWrapper>
+
+    <ScrollephantNavigationsWrapper>
+        <ScrollephantArrowPrev />
+        <ScrollephantDots />
+        <ScrollephantArrowNext />
+    </ScrollephantNavigationsWrapper>
+</Scrollephant>
