cb4f6519354a17753d089b717b22c63ded408545
babakfp <babak.bxf@gmail.com>
2023-07-21 09:06:41 -0700

    Added demos

---
+++ src/routes/horizontal-non-scrollable-subsections/+page.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+    import {
+        Scrollephant,
+        ScrollephantWrapper,
+        ScrollephantSection,
+        ScrollephantSubSection,
+    } from "scrollephant"
+</script>
+
+<Scrollephant direction="horizontal" scrollableSubSections={false}>
+    <ScrollephantWrapper>
+        <ScrollephantSection label="Section 1">1</ScrollephantSection>
+        <ScrollephantSection label="Section 2">
+            <ScrollephantWrapper>
+                <ScrollephantSubSection label="Section 2.1">
+                    2.1
+                </ScrollephantSubSection>
+                <ScrollephantSubSection label="Section 2.2">
+                    2.2
+                </ScrollephantSubSection>
+                <ScrollephantSubSection label="Section 2.3">
+                    2.3
+                </ScrollephantSubSection>
+            </ScrollephantWrapper>
+        </ScrollephantSection>
+        <ScrollephantSection label="Section 3">3</ScrollephantSection>
+    </ScrollephantWrapper>
+</Scrollephant>
