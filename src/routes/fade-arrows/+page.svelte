f7c1aff15decabde6dbb6a8922b8f97ca8f139cc
babakfp <babak.bxf@gmail.com>
2023-07-20 19:52:10 -0700

    Added demos

---
+++ src/routes/fade-arrows/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    import {
+        Scrollephant,
+        ScrollephantWrapper,
+        ScrollephantSection,
+        ScrollephantNavigationsWrapper,
+        ScrollephantArrowPrev,
+        ScrollephantArrowNext,
+    } from "scrollephant"
+</script>
+
+<Scrollephant movement="fade">
+    <ScrollephantWrapper>
+        <ScrollephantSection label="Section 1">1</ScrollephantSection>
+        <ScrollephantSection label="Section 2">2</ScrollephantSection>
+        <ScrollephantSection label="Section 3">3</ScrollephantSection>
+    </ScrollephantWrapper>
+
+    <ScrollephantNavigationsWrapper>
+        <ScrollephantArrowPrev />
+        <ScrollephantArrowNext />
+    </ScrollephantNavigationsWrapper>
+</Scrollephant>
