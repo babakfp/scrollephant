771563ea10cd4e09ab35fbf6ce59c5c24558415a
babakfp <babak.bxf@gmail.com>
2023-12-31 22:57:56 -0800

    .

--- src/routes/demo/Controls.svelte
+++ src/routes/demo/Controls.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     import type { Writable } from "svelte/store"
     import type { ControlsType } from "./types.js"
+    import { persisted } from "svelte-persisted-store"
     import {
         movement,
         direction,
@@ -13,161 +14,165 @@
 
     export let controls: Writable<ControlsType>
 
-    let isControlsOpen = false
+    let isControlsOpen = persisted("isControlsOpen", false)
 
     controls.subscribe(_controls => {
         $sections = $sections.map((section, i) => {
             section.label = _controls.sections[i].label
             section.autoHeight = _controls.sections[i].autoHeight
-            section.subsections = section.subsections.map((subsection, i2) => {
-                section.label = _controls.sections[i].subsections[i2].label
-                section.autoHeight =
-                    _controls.sections[i].subsections[i2].autoHeight
-                return subsection
-            })
+            // section.subsections = section.subsections.map((subsection, i2) => {
+            //     section.label = _controls.sections[i].subsections[i2].label
+            //     section.autoHeight =
+            //         _controls.sections[i].subsections[i2].autoHeight
+            //     return subsection
+            // })
             return section
         })
     })
 
-    const buttonClasses =
-        "rounded-md bg-indigo-600 px-3.5 h-10 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+    const insertItemAtIndex = (array: any[], item: any, index: number) => {
+        // Create a copy of the original array to avoid modifying it directly
+        const newArray = [...array]
+        // Use splice to insert the item at the specified index
+        newArray.splice(index, 0, item)
+        // Return the modified array
+        return newArray
+    }
 </script>
 
-<div class="fixed top-0 left-0 bottom-0 p-4 grid gap-4">
+<div class="fixed top-0 left-0 bottom-0 p-4 grid gap-4 text-sm">
     <button
-        class="justify-self-start {buttonClasses}"
-        on:click={() => (isControlsOpen = !isControlsOpen)}>Settings</button
+        class="justify-self-start"
+        on:click={() => ($isControlsOpen = !$isControlsOpen)}>Settings</button
     >
 
-    {#if isControlsOpen}
-        <div class="bg-white rounded-md grid gap-4 h-full overflow-y-auto p-4">
-            <ul class="grid gap-4">
-                <li class="p-4 bg-gray-100 rounded-md">
-                    <p><code>{"<Scrollephant />"}</code> options</p>
-                    <ul class="mt-4">
-                        <li>
-                            <select bind:value={$movement}>
-                                <option value="scroll">Scroll</option>
-                                <option value="fade">Fade</option>
-                            </select>
-                        </li>
-                        <li>
-                            <select bind:value={$direction}>
-                                <option value="vertical">Vertical</option>
-                                <option value="horizontal">Horizontal</option>
-                            </select>
-                        </li>
-                        <li>
+    {#if $isControlsOpen}
+        <div class="bg-white rounded-md h-full overflow-y-auto p-4">
+            <p><code>{"<Scrollephant />"}</code> options</p>
+            <div class="p-4 bg-gray-50 rounded-md mt-2">
+                <select bind:value={$movement}>
+                    <option value="scroll">Scroll</option>
+                    <option value="fade">Fade</option>
+                </select>
+                <select class="mt-2" bind:value={$direction}>
+                    <option value="vertical">Vertical</option>
+                    <option value="horizontal">Horizontal</option>
+                </select>
+                <label class="mt-4">
+                    <input type="checkbox" bind:checked={$loopUp} />
+                    <span>Loop Up</span>
+                </label>
+                <label class="mt-2">
+                    <input type="checkbox" bind:checked={$loopDown} />
+                    <span>Loop Down</span>
+                </label>
+                <label class="mt-2">
+                    <input type="checkbox" bind:checked={$restrictMovement} />
+                    <span>Restrict Movement</span>
+                </label>
+                <label class="mt-2">
+                    <input
+                        type="checkbox"
+                        bind:checked={$scrollableSubsections}
+                    />
+                    <span>Scrollable Sub Sections</span>
+                </label>
+            </div>
+
+            <div class="grid gap-4 mt-4">
+                {#each $controls.sections as section, sectionIndex}
+                    <p>Section</p>
+
+                    <div class="grid gap-4 p-4 bg-gray-50 rounded-md">
+                        <label>
+                            <span>Label</span>
+                            <input type="text" bind:value={section.label} />
+                        </label>
+                        <label>
+                            <input
+                                type="checkbox"
+                                bind:checked={section.autoHeight}
+                            />
+                            <span>Auto Height</span>
+                        </label>
+                        <label>
+                            <span>Text Content</span>
+                            <input
+                                type="text"
+                                bind:value={section.textContent}
+                            />
+                        </label>
+                    </div>
+
+                    {#each section.subsections as subsection}
+                        <p class="ml-8">Subsection</p>
+
+                        <div class="grid gap-4 p-4 bg-gray-50 rounded-md ml-8">
                             <label>
-                                <input type="checkbox" bind:checked={$loopUp} />
-                                <span>Loop Up</span>
-                            </label>
-                        </li>
-                        <li>
-                            <label>
-                                <input
-                                    type="checkbox"
-                                    bind:checked={$loopDown}
-                                />
-                                <span>Loop Down</span>
-                            </label>
-                        </li>
-                        <li>
-                            <label>
-                                <input
-                                    type="checkbox"
-                                    bind:checked={$restrictMovement}
-                                />
-                                <span>Restrict Movement</span>
-                            </label>
-                        </li>
-                        <li>
-                            <label>
-                                <input
-                                    type="checkbox"
-                                    bind:checked={$scrollableSubsections}
-                                />
-                                <span>Scrollable Sub Sections</span>
-                            </label>
-                        </li>
-                    </ul>
-                </li>
-                <li>
-                    <ul>
-                        {#each $controls.sections as section}
-                            <li>
-                                <label>Label</label>
-                                <input type="text" bind:value={section.label} />
-                            </li>
-                            <li>
-                                <div>
-                                    <input
-                                        type="checkbox"
-                                        bind:checked={section.autoHeight}
-                                    />
-                                    <span>Auto Height</span>
-                                </div>
-                            </li>
-                            <li>
-                                <label>Text Content</label>
+                                <span>Label</span>
                                 <input
                                     type="text"
-                                    bind:value={section.textContent}
+                                    bind:value={subsection.label}
                                 />
-                            </li>
-                            {#if section?.subsections?.length}
-                                <li>
-                                    <ul>
-                                        {#each section.subsections as subsection}
-                                            <li>
-                                                <label>Label</label>
-                                                <input
-                                                    type="text"
-                                                    bind:value={subsection.label}
-                                                />
-                                            </li>
-                                            <li>
-                                                <div>
-                                                    <input
-                                                        type="checkbox"
-                                                        bind:checked={subsection.autoHeight}
-                                                    />
-                                                    <span>Auto Height</span>
-                                                </div>
-                                            </li>
-                                            <li>
-                                                <label>Text Content</label>
-                                                <input
-                                                    type="text"
-                                                    bind:value={subsection.textContent}
-                                                />
-                                            </li>
-                                        {/each}
-                                    </ul>
-                                </li>
-                            {/if}
-                            <button
-                                class={buttonClasses}
-                                on:click={() => {
-                                    const newSubsection = {
-                                        label: "",
+                            </label>
+                            <label>
+                                <input
+                                    type="checkbox"
+                                    bind:checked={subsection.autoHeight}
+                                />
+                                <span>Auto Height</span>
+                            </label>
+                            <label>
+                                <span>Text Content</span>
+                                <input
+                                    type="text"
+                                    bind:value={subsection.textContent}
+                                />
+                            </label>
+                        </div>
+                    {/each}
+                    <!-- <button
+                        on:click={() => {
+                            const newSubsection = {
+                                label: "",
+                                autoHeight: false,
+                                textContent: "",
+                                subsections: [],
+                            }
+                            if (!section?.subsections) {
+                                section.subsections = [newSubsection]
+                            } else {
+                                section.subsections.push(newSubsection)
+                            }
+                        }}
+                    >
+                        Add subsection
+                    </button> -->
+
+                    <button
+                        on:click={() => {
+                            const newSectionDestinationIndex = sectionIndex + 1
+                            controls.update(_controls => {
+                                _controls.sections = insertItemAtIndex(
+                                    _controls.sections,
+                                    {
+                                        label: "New section",
                                         autoHeight: false,
-                                        textContent: "",
+                                        textContent: "New section",
                                         subsections: [],
-                                    }
-                                    if (!section?.subsections) {
-                                        section.subsections = [newSubsection]
-                                    } else {
-                                        section.subsections.push(newSubsection)
-                                    }
-                                }}
-                            >
-                                Add subsection
-                            </button>
-                        {/each}
-                    </ul>
-                </li>
-                <li>
+                                    },
+                                    newSectionDestinationIndex
+                                )
+                                return _controls
+                            })
+                        }}
+                    >
+                        Add section
+                    </button>
+                {/each}
+            </div>
+
+            <!-- <li>
                     <ul>
                         <li>
                             <label>
@@ -206,26 +211,33 @@
                             </label>
                         </li>
                     </ul>
-                </li>
-            </ul>
+                </li> -->
         </div>
     {/if}
 </div>
 
 <style lang="postcss">
-    input,
+    button {
+        @apply rounded-md bg-indigo-600 px-3.5 h-10 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600;
+    }
+    input:not([type="checkbox"]),
     select {
-        @apply h-10 rounded-md px-3.5;
+        @apply h-10 rounded-md px-3.5 border-2 border-gray-200;
     }
     input:not([type="checkbox"]),
     select {
         width: 100%;
     }
-    ul {
-        display: grid;
-        gap: 1rem;
+    select {
+        @apply appearance-none cursor-pointer;
     }
-    li > label {
-        @apply flex gap-4 items-center;
+    input[type="checkbox"] {
+        @apply w-4 h-4;
+    }
+    label:has(input[type="checkbox"]) {
+        @apply flex gap-2 items-center cursor-pointer;
+    }
+    label:not(:has(input[type="checkbox"])) {
+        @apply grid gap-0.5;
     }
 </style>
