2887adf77c63b72784bc6a09942d3c34d613b088
babakfp <babak.bxf@gmail.com>
2024-03-25 22:36:25 +0330

    .

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,7 @@
 <script lang="ts">
     import {
         Scrollephant,
-        ScrollephantWrapper,
         ScrollephantSection,
-        ScrollephantSubsection,
-        ScrollephantNavigationsWrapper,
         ScrollephantArrowPrev,
         ScrollephantArrowNext,
         ScrollephantDots,
@@ -12,27 +9,35 @@
 </script>
 
 <Scrollephant>
-    <ScrollephantWrapper>
+    <svelte:fragment slot="sections">
         <ScrollephantSection label="Section 1">1</ScrollephantSection>
-        <ScrollephantSection label="Section 2">
-            <ScrollephantWrapper>
-                <ScrollephantSubsection label="Section 2.1">
-                    2.1
-                </ScrollephantSubsection>
-                <ScrollephantSubsection label="Section 2.2">
-                    2.2
-                </ScrollephantSubsection>
-                <ScrollephantSubsection label="Section 2.3">
-                    2.3
-                </ScrollephantSubsection>
-            </ScrollephantWrapper>
-        </ScrollephantSection>
-        <ScrollephantSection label="Section 3">3</ScrollephantSection>
-    </ScrollephantWrapper>
 
-    <ScrollephantNavigationsWrapper>
+        <ScrollephantSection label="Section 2">
+            <svelte:fragment slot="subsections">
+                <ScrollephantSection label="Section 2.1">
+                    2.1
+                </ScrollephantSection>
+                <ScrollephantSection label="Section 2.2">
+                    2.2
+                </ScrollephantSection>
+                <ScrollephantSection label="Section 2.3">
+                    2.3
+                </ScrollephantSection>
+            </svelte:fragment>
+
+            <svelte:fragment slot="navigation">
+                <ScrollephantArrowPrev />
+                <ScrollephantDots />
+                <ScrollephantArrowNext />
+            </svelte:fragment>
+        </ScrollephantSection>
+
+        <ScrollephantSection label="Section 3">3</ScrollephantSection>
+    </svelte:fragment>
+
+    <svelte:fragment slot="navigation">
         <ScrollephantArrowPrev />
         <ScrollephantDots />
         <ScrollephantArrowNext />
-    </ScrollephantNavigationsWrapper>
+    </svelte:fragment>
 </Scrollephant>
